b05f37e643c96f6f311a317ce2c040e8e11deeb9
Astorec <rossa1298@gmail.com>
2025-10-26 22:33:33 +0000

    Default COnfig

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports ={
+    database:{
+        connectionLimit: 10,
+        host: "", // IP or hostname
+        user: "", // database user
+        password: "", // database password
+        database: "", // database name
+
+    },
+    server: {
+        port: 3000, // server port
+        address: "", // server address
+        sslKeyPath: "", // path to ssl key
+        sslCertPath: "", // path to ssl certificate
+    }
+}
